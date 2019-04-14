f14fe1df4b119fd83339dca676bfc1fdd68680ac
SkyWaiting <guorui_016@aliyun.com>
2019-04-14 08:21:46 +0800

    整合代码

--- ch2/tempconv/tempconv.go
+++ ch2/tempconv/tempconv.go
@@ -2,15 +2,19 @@ package tempconv
 
 import "fmt"
 
-type Celsius float64  // 摄氏温度
-type Fahrenheit float64  //华氏温度
+type Celsius float64    // 摄氏温度
+type Fahrenheit float64 //华氏温度
 
 const (
-	AbsoluteZeroC Celsius = -273.15  // 绝对零度
-	FreezingC Celsius = 0  // 结冰点温度
-	BoilingC Celsius = 100  //沸水温度
+	AbsoluteZeroC Celsius = -273.15 // 绝对零度
+	FreezingC     Celsius = 0       // 结冰点温度
+	BoilingC      Celsius = 100     //沸水温度
 )
 
 func (c Celsius) String() string {
 	return fmt.Sprintf("%g°C", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%g°F", f)
 }
